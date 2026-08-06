37c46813613b525916a79a0d14d144380941d1df
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2026-08-06 16:32:19 -0400

    completed challenge2

--- ref/src/main.jsx
+++ ref/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import App from '../src/challenge1/App.jsx';
+import App from '../src/challenge2/App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+/**
+ * Challenge 2 of 4: Focus the search field 
+Make it so that clicking the “Search” button puts focus into the field.
+ */
